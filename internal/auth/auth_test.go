401a5653c930e82ad940aaf7fb679d83c12b1263
ShichiNoBushi <shichinobushi@gmail.com>
2025-08-08 14:38:58 -0500

    Added tests with broken test to fail.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey1(t *testing.T) {
+	testHeaders := make(http.Header)
+
+	testHeaders.Set("Authorization", "ApiKey testing")
+
+	got, err := GetAPIKey(testHeaders)
+	want := "testing"
+
+	if err != nil {
+		t.Fatalf("expected: nil error, got: %s", err)
+	}
+
+	if got == want { //restore to got != want
+		t.Fatalf("expected: %s, got: %s", want, got)
+	}
+}
+
+func TestGetAPIKey2(t *testing.T) {
+	testHeaders := make(http.Header)
+
+	testHeaders.Set("Authorization", "")
+
+	_, err := GetAPIKey(testHeaders)
+
+	if err == nil {
+		t.Fatalf("expected: an error, got: %s", err)
+	}
+
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("expected: no authorization error, got: %s", err)
+	}
+}
+
+func TestGetAPIKey3(t *testing.T) {
+	testHeaders := make(http.Header)
+
+	testHeaders.Set("Authorization", "incorrect")
+
+	_, err := GetAPIKey(testHeaders)
+
+	if err == nil {
+		t.Fatalf("expected: an error, got: %s", err)
+	}
+
+	if err.Error() != "malformed authorization header" {
+		t.Fatalf("expected: invalid authorization error, got: %s", err)
+	}
+}
